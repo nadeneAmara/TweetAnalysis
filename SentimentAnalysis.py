e139f0c2abe83e0e9d84e1ae3d3e0674dd8617ee
nadeneAmara <nadeneabuamara@gmail.com>
2020-03-06 22:59:02 -0800

    added function for building test set

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,10 +1,18 @@
 import twitter
 
 # initialize API instance
-twitter_api = twitter.Api(consumer_key= TXX58RuQCDKrR6WKOrDSuhhTp,
-						 consumer_secret= llAoZ4hA0HPOUg3wVhLVL5beULvn9nYkmrErhB1YaxwXzpm9od,
-						 access_token_key= 4635485808-5TgOrcP6MPM6mWxHygSM9KR8YNLAryWFlDs1S2c,
-						 access_token_secret= c0XtZEzEBGsW1I0PzbdX4Y1k3ZgmMj82mMSvrIz4dSo1r)
+twitter_api = twitter.Api(consumer_key= 'TXX58RuQCDKrR6WKOrDSuhhTp',
+						 consumer_secret= 'llAoZ4hA0HPOUg3wVhLVL5beULvn9nYkmrErhB1YaxwXzpm9od',
+						 access_token_key= '4635485808-5TgOrcP6MPM6mWxHygSM9KR8YNLAryWFlDs1S2c',
+						 access_token_secret= 'c0XtZEzEBGsW1I0PzbdX4Y1k3ZgmMj82mMSvrIz4dSo1r')
 
-# test authentication
-print(twitter_api.VerifyCredentials())
+def buildTestSet(search_keyword): 
+	try:
+		tweets_returned = twitter_api.GetSearch(search_keyword, count = 100)
+		print("Returned" + str(len(tweets_returned)) + " tweets for the term " + search_keyword)
+
+		return [{"text":status.text, "label":None} for status in tweets_returned]
+
+	except:
+		print("Something went wrong")
+		return None
